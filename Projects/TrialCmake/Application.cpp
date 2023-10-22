8ddaaa5f0b088f4d19de11c085e0658f003f034f
Aditya <adi2tya98@gmail.com>
2023-10-22 11:32:01 +0530

    Added config files and Defined Cmake options

--- Projects/TrialCmake/Application.cpp
+++ Projects/TrialCmake/Application.cpp
@@ -1,21 +1,29 @@
 #ifndef __IOSTREAM_H
 #include<iostream>
 #endif
-#include "mathLib.h"
+
 #include "GLFW/glfw3.h"
 #include "People.cpp"
 #include "Shape.cpp"
+#include "build/Config.h"
+#ifdef USE_MATHLIB
+#include "mathLib.h"
+#endif
 using namespace std;
 
 int main(){
+
+    #ifdef USE_MATHLIB
+
+    printf("\n\tUsing user defined mathlib for circle");
     mathLib::Circle *obj1 = new mathLib::Circle(10);
     cout<<"\n\tArea of circle = "<<obj1->getArea();
     cout<<"\n\tCircumference of circle = "<<obj1->getCircumference();
     cout<<"\n\t3.5 + 4.5 = "<<mathLib::add(3.5f,4.5f)<<endl; 
 
-    people::person *person1 = new people::person("Testing", 23, "IN");
-    person1->displayInfo();
-
+    #else
+    
+    printf("\n\tNot using user defined mathlib, instead using Shapes as circle");
     Shapes::Circle *circle = new Shapes::Circle(10);
     cout<<"Area of circle = "<<circle->area()<<endl;
 
@@ -25,6 +33,11 @@ int main(){
     Shapes::Triangle *tria = new Shapes::Triangle(4,3);
     cout<<"Area of traingle = "<<tria->area()<<endl;
     cout<<"Perimeter of triangle = "<<tria->perimeter()<<endl;
+
+    #endif
+
+    people::person *person1 = new people::person("Testing", 23, "IN");
+    person1->displayInfo();
 
 GLFWwindow *window;
 
